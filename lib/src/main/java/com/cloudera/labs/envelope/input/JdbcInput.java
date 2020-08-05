c3951de9f63175dacd45149d486518536c28e37a
marek.macura <marek.macura@protonmail.ch>
2020-08-05 13:27:48 +0200

    Upgrade to Spark 2.4.5, JDBC driver included in jar, Spark JDBC option enhancement.

--- lib/src/main/java/com/cloudera/labs/envelope/input/JdbcInput.java
+++ lib/src/main/java/com/cloudera/labs/envelope/input/JdbcInput.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2019, Cloudera, Inc. All Rights Reserved.
+ * Copyright (c) 2015-2020, Cloudera, Inc. All Rights Reserved.
  *
  * Cloudera, Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"). You may not use this file except in
@@ -32,11 +32,19 @@ public class JdbcInput implements BatchInput, ProvidesAlias, ProvidesValidations
   public static final String JDBC_CONFIG_TABLENAME = "tablename";
   public static final String JDBC_CONFIG_USERNAME = "username";
   public static final String JDBC_CONFIG_PASSWORD = "password";
+  public static final String JDBC_CONFIG_PARTITIONCOLUMN ="partitioncolumn";
+  public static final String JDBC_CONFIG_LOWERBOUND = "lowerbound";
+  public static final String JDBC_CONFIG_UPPERBOUND = "upperbound";
+  public static final String JDBC_CONFIG_NUMPARTITIONS = "numpartitions";
 
   private String url;
   private String tableName;
   private String username;
   private String password;
+  private String partitionColumn;
+  private long lowerBound;
+  private long upperBound;
+  private int numPartitions;
 
   @Override
   public void configure(Config config) {
@@ -44,6 +52,10 @@ public class JdbcInput implements BatchInput, ProvidesAlias, ProvidesValidations
     this.tableName = config.getString(JDBC_CONFIG_TABLENAME);
     this.username = config.getString(JDBC_CONFIG_USERNAME);
     this.password = config.getString(JDBC_CONFIG_PASSWORD);
+    this.partitionColumn = config.getString(JDBC_CONFIG_PARTITIONCOLUMN);
+    this.lowerBound = config.getLong(JDBC_CONFIG_LOWERBOUND);
+    this.upperBound = config.getLong(JDBC_CONFIG_UPPERBOUND);
+    this.numPartitions = config.getInt(JDBC_CONFIG_NUMPARTITIONS);
   }
 
   @Override
@@ -51,8 +63,9 @@ public class JdbcInput implements BatchInput, ProvidesAlias, ProvidesValidations
     Properties properties = new Properties();
     properties.put("user", username);
     properties.put("password", password);
+    properties.put("driver","com.microsoft.sqlserver.jdbc.SQLServerDriver");
 
-    return Contexts.getSparkSession().read().jdbc(url, tableName, properties);
+    return Contexts.getSparkSession().read().jdbc(url, tableName, partitionColumn,lowerBound,upperBound,numPartitions, properties);
   }
 
   @Override
@@ -67,6 +80,10 @@ public class JdbcInput implements BatchInput, ProvidesAlias, ProvidesValidations
         .mandatoryPath(JDBC_CONFIG_TABLENAME, ConfigValueType.STRING)
         .mandatoryPath(JDBC_CONFIG_USERNAME, ConfigValueType.STRING)
         .mandatoryPath(JDBC_CONFIG_PASSWORD, ConfigValueType.STRING)
+        .mandatoryPath(JDBC_CONFIG_PARTITIONCOLUMN, ConfigValueType.STRING)
+        .mandatoryPath(JDBC_CONFIG_UPPERBOUND, ConfigValueType.NUMBER)
+        .mandatoryPath(JDBC_CONFIG_LOWERBOUND, ConfigValueType.NUMBER)
+        .mandatoryPath(JDBC_CONFIG_NUMPARTITIONS, ConfigValueType.NUMBER)
         .allowEmptyValue(JDBC_CONFIG_PASSWORD)
         .build();
   }
